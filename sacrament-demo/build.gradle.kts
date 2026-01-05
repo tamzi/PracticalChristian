6b30fef91ee45770de5f4e589fdce3d8e796489f
./root <tamrefrank@gmail.com>
2026-01-05 22:07:46 +0100

    Created sacrament-demo build configuration

---
+++ sacrament-demo/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.practicalchristian.android.application)
+    alias(libs.plugins.practicalchristian.android.application.compose)
+}
+
+android {
+    namespace = "com.sacrament.demo"
+
+    defaultConfig {
+        applicationId = "com.sacrament.demo"
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/*"
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.sacrament)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.runtime)
+}
+
