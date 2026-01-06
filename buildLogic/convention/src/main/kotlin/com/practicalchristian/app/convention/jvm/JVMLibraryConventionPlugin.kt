457ecbc25d0b1055986cbaa8fc4fb3ea9e0f2643
./root <tamrefrank@gmail.com>
2026-01-06 20:14:29 +0100

    Added JUnit Platform to convention plugins

--- buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/jvm/JVMLibraryConventionPlugin.kt
+++ buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/jvm/JVMLibraryConventionPlugin.kt
@@ -3,6 +3,8 @@ package com.practicalchristian.app.convention.jvm
 import com.practicalchristian.app.convention.android.configureKotlinJvm
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
+import org.gradle.kotlin.dsl.withType
 
 class JvmLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -11,6 +13,11 @@ class JvmLibraryConventionPlugin : Plugin<Project> {
                 apply("org.jetbrains.kotlin.jvm")
             }
             configureKotlinJvm()
+            
+            // Configure test tasks to use JUnit Platform for JUnit 6 (Jupiter)
+            tasks.withType<Test> {
+                useJUnitPlatform()
+            }
         }
     }
 }
