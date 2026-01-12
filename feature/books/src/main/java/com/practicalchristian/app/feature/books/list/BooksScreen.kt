8de379ca59e8acfb5e58722b896787988515f171
./root <tamrefrank@gmail.com>
2026-01-12 07:11:49 +0100

    Removed unnecessary ExperimentalMaterial3Api opt-ins

--- feature/books/src/main/java/com/practicalchristian/app/feature/books/list/BooksScreen.kt
+++ feature/books/src/main/java/com/practicalchristian/app/feature/books/list/BooksScreen.kt
@@ -6,31 +6,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.List
 import androidx.compose.material.icons.rounded.GridOn
 import androidx.compose.material.icons.rounded.Warning
-import com.sacrament.ui.components.action.SacramentIconButton
-import com.sacrament.ui.components.feedback.SacramentProgressIndicator
-import com.sacrament.ui.components.feedback.SacramentProgressVariant
-import com.sacrament.ui.components.navigation.SacramentTopAppBar
-import com.sacrament.ui.components.surface.SacramentCard
-import com.sacrament.ui.components.surface.SacramentCardDefaults
-import com.sacrament.ui.patterns.SacramentScreenScaffold
-import com.sacrament.ui.primitives.SacramentIcon
-import com.sacrament.ui.primitives.SacramentText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -44,11 +34,20 @@ import com.practicalchristian.app.core.ui.helpers.ViewType
 import com.practicalchristian.app.core.ui.helpers.sentence
 import com.practicalchristian.app.core.ui.navigation.AppDestination
 import com.practicalchristian.app.core.ui.navigation.AppNavigator
-import com.sacrament.ui.primitives.SacramentCenteredColumn
-import com.sacrament.ui.primitives.SacramentDivider
-import com.sacrament.ui.patterns.SacramentEmptyState
+import com.sacrament.ui.components.action.SacramentIconButton
+import com.sacrament.ui.components.feedback.SacramentProgressIndicator
+import com.sacrament.ui.components.feedback.SacramentProgressVariant
+import com.sacrament.ui.components.navigation.SacramentTopAppBar
+import com.sacrament.ui.components.surface.SacramentCard
+import com.sacrament.ui.components.surface.SacramentCardDefaults
 import com.sacrament.ui.foundation.Bar
 import com.sacrament.ui.foundation.SacramentTheme
+import com.sacrament.ui.patterns.SacramentEmptyState
+import com.sacrament.ui.patterns.SacramentScreenScaffold
+import com.sacrament.ui.primitives.SacramentCenteredColumn
+import com.sacrament.ui.primitives.SacramentDivider
+import com.sacrament.ui.primitives.SacramentIcon
+import com.sacrament.ui.primitives.SacramentText
 
 /**
  * Books screen - List of books.
@@ -214,7 +213,6 @@ fun BooksScreenContent(
 }
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
 private fun BookItem(
     isGrid: Boolean,
     book: Book,
