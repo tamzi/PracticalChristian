83c55dc649a41e59c71802799b81c9425fce53d7
./root <tamrefrank@gmail.com>
2026-01-14 07:31:35 +0100

    moved buttonVariants screen

--- sacrament-demo/src/main/java/com/sacrament/demo/action/button/ButtonVariantsCatalogScreen.kt
+++ sacrament-demo/src/main/java/com/sacrament/demo/action/button/ButtonVariantsCatalogScreen.kt
@@ -1,4 +1,4 @@
-package com.sacrament.demo.action.button.screens
+package com.sacrament.demo.action.button
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
