d1f386510eeadce20fb49008b97ed0fe04c08984
./root <tamrefrank@gmail.com>
2026-01-15 05:10:08 +0100

    Moved ButtonCatalogScreen to ButtonCatalogOverviewScreen

--- sacrament-demo/src/main/java/com/sacrament/demo/action/button/ButtonCatalogOverviewScreen.kt
+++ sacrament-demo/src/main/java/com/sacrament/demo/action/button/ButtonCatalogOverviewScreen.kt
@@ -1,4 +1,4 @@
-package com.sacrament.demo.action.button.screens
+package com.sacrament.demo.action.button
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
