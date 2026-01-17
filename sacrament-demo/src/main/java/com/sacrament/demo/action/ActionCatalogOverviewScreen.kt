bc234318e89383670303dd13bafab76b6b7caad8
./root <tamrefrank@gmail.com>
2026-01-17 22:02:14 +0100

    added sematicRole

--- sacrament-demo/src/main/java/com/sacrament/demo/action/ActionCatalogOverviewScreen.kt
+++ sacrament-demo/src/main/java/com/sacrament/demo/action/ActionCatalogOverviewScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import com.sacrament.demo.CatalogSectionTitle
 import com.sacrament.demo.CatalogTopAppBar
 import com.sacrament.ui.foundation.SacramentTheme
@@ -104,7 +105,10 @@ private fun ActionCatalogListItem(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable(onClick = onClick)
+            .clickable(
+                role = Role.Button,
+                onClick = onClick
+            )
             .padding(vertical = SacramentTheme.spacing.md)
     ) {
         CatalogSectionTitle(text = title)
