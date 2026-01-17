5d68194626e43ef0742433cae1c8e6cbb5835cde
./root <tamrefrank@gmail.com>
2026-01-17 22:10:26 +0100

    create list item with icon

--- sacrament/src/main/java/com/sacrament/ui/components/content/SacramentListItem.kt
+++ sacrament/src/main/java/com/sacrament/ui/components/content/SacramentListItem.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sacrament.ui.foundation.Bar
 import com.sacrament.ui.foundation.SacramentTheme
 import com.sacrament.ui.primitives.SacramentText
@@ -81,6 +86,27 @@ private fun SacramentListItemNoLeadingPreview() {
         Surface(color = Color.White) {
             SacramentListItem(
                 headline = { SacramentText(text = "List item without leading icon") },
+                supporting = { SacramentText(text = "Supporting text", style = SacramentTheme.typography.bodySmall) },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SacramentListItemWithIconPreview() {
+    SacramentTheme(navigationBar = Bar.SURFACE, statusBar = Bar.BACKGROUND) {
+        Surface(color = Color.White) {
+            SacramentListItem(
+                leading = {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = "Settings",
+                        modifier = Modifier.size(24.dp),
+                        tint = Color.Gray
+                    )
+                },
+                headline = { SacramentText(text = "List item with icon") },
                 supporting = { SacramentText(text = "Supporting text", style = SacramentTheme.typography.bodySmall) },
             )
         }
