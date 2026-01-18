036610f66ea8d4e33f6faceb4045a69ab769b820
./root <tamrefrank@gmail.com>
2026-01-18 19:40:46 +0100

    updated Renamed SacramentIcons in ssacramentUI

--- sacrament/src/main/java/com/sacrament/ui/components/input/SacramentSearchField.kt
+++ sacrament/src/main/java/com/sacrament/ui/components/input/SacramentSearchField.kt
@@ -1,25 +1,25 @@
 package com.sacrament.ui.components.input
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 
 /**
  * Search input built on SacramentTextField.
  */
 @Composable
 fun SacramentSearchField(
+    modifier: Modifier = Modifier,
     query: String,
     onQueryChange: (String) -> Unit,
     onSearch: ((String) -> Unit)? = null,
     size: SacramentTextFieldSize = SacramentTextFieldSize.Medium,
     enabled: Boolean = true,
     placeholder: String = "Search",
-    modifier: Modifier = Modifier,
 ) {
     SacramentTextField(
         value = query,
