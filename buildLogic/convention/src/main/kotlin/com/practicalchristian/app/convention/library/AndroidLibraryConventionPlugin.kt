7df93a77d44cb8944804664924410c0500570cc9
./root <tamrefrank@gmail.com>
2026-01-21 07:31:27 +0100

    updated library convention plugin

--- buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/library/AndroidLibraryConventionPlugin.kt
+++ buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/library/AndroidLibraryConventionPlugin.kt
@@ -19,7 +19,6 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.library")
-                apply("org.jetbrains.kotlin.android")
             }
 
             extensions.configure<LibraryExtension> {
