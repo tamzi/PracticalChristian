e324e0cae5e934b82f1e41ac1558d0905f4b504c
./root <tamrefrank@gmail.com>
2026-01-21 07:30:32 +0100

    updated test convention plugin

--- buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/test/AndroidTestConventionPlugin.kt
+++ buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/test/AndroidTestConventionPlugin.kt
@@ -21,7 +21,6 @@ class AndroidTestConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.test")
-                apply("org.jetbrains.kotlin.android")
                 apply("practicalchristian.jacoco") // Use our JaCoCo convention plugin
             }
 
