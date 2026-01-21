a79d494a4b4c055d111d60a5c2ecf1b769878061
./root <tamrefrank@gmail.com>
2026-01-21 07:30:52 +0100

    updated android convention plugin

--- buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/android/AndroidCompose.kt
+++ buildLogic/convention/src/main/kotlin/com/practicalchristian/app/convention/android/AndroidCompose.kt
@@ -7,27 +7,18 @@ import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
-internal fun Project.configureAndroidCompose(commonExtension: CommonExtension<*, *, *, *, *, *>) {
-    commonExtension.apply {
-        buildFeatures {
-            compose = true
-        }
+internal fun Project.configureAndroidCompose(commonExtension: CommonExtension) {
+    commonExtension.buildFeatures.compose = true
 
-        dependencies {
-            val bom = libs.findLibrary("androidx-compose-bom").get()
-            add("implementation", platform(bom))
-            add("androidTestImplementation", platform(bom))
-            add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
-            add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
-        }
-
-        testOptions {
-            unitTests {
-                // For Robolectric
-                isIncludeAndroidResources = true
-            }
-        }
+    dependencies {
+        val bom = libs.findLibrary("androidx-compose-bom").get()
+        add("implementation", platform(bom))
+        add("androidTestImplementation", platform(bom))
+        add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+        add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
     }
+
+    commonExtension.testOptions.unitTests.isIncludeAndroidResources = true
 
     extensions.configure<ComposeCompilerGradlePluginExtension> {
         fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
