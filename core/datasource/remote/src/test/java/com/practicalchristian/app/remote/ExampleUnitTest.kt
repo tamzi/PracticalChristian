c0c25f4eaac56f7b3060aea79e8c71e6dc4c4410
./root <tamrefrank@gmail.com>
2026-01-22 05:59:56 +0100

    removed old tests as they dont serve  apurpose

--- core/datasource/remote/src/test/java/com/practicalchristian/app/remote/ExampleUnitTest.kt
+++ core/datasource/remote/src/test/java/com/practicalchristian/app/remote/ExampleUnitTest.kt
@@ -1,16 +1,20 @@
 package com.practicalchristian.app.remote
 
+/*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
+*/
 /**
  * Example local unit test using JUnit 6 (Jupiter)
- * 
+ *
  * See [testing documentation](http://d.android.com/tools/testing)
- */
+ *//*
+
 class ExampleUnitTest {
     @Test
     fun `addition is correct`() {
         assertEquals(4, 2 + 2)
     }
 }
+*/
