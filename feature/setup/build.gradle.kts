13bcdc07d09ae856cbd25633681fe07f5c19b5b3
./root <tamrefrank@gmail.com>
2026-02-07 13:48:12 +0100

    updated feature to use configure ApplicationExtension p5

--- feature/setup/build.gradle.kts
+++ feature/setup/build.gradle.kts
@@ -1,7 +1,9 @@
+import com.android.build.api.dsl.LibraryExtension
+
 plugins {
     alias(libs.plugins.practicalchristian.android.feature)
 }
 
-android {
+configure<LibraryExtension> {
     namespace = "com.practicalchristian.app.feature.setup"
 }
