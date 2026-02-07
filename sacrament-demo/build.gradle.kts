8bdd0b2b9e8d5206c2bed44de78b382640473d67
./root <tamrefrank@gmail.com>
2026-02-07 13:46:10 +0100

    updated sacDemo to use configure<ApplicationExtension>

--- sacrament-demo/build.gradle.kts
+++ sacrament-demo/build.gradle.kts
@@ -1,9 +1,11 @@
+import com.android.build.api.dsl.ApplicationExtension
+
 plugins {
     alias(libs.plugins.practicalchristian.android.application)
     alias(libs.plugins.practicalchristian.android.application.compose)
 }
 
-android {
+configure<ApplicationExtension> {
     namespace = "com.sacrament.demo"
 
     defaultConfig {
