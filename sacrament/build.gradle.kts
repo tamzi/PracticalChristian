cabe32f92ecb7a25167183c9ca0a60a121c94d85
./root <tamrefrank@gmail.com>
2026-02-07 13:46:23 +0100

    updated sacrament to use configure<ApplicationExtension>

--- sacrament/build.gradle.kts
+++ sacrament/build.gradle.kts
@@ -1,10 +1,12 @@
+import com.android.build.api.dsl.LibraryExtension
+
 plugins {
     alias(libs.plugins.practicalchristian.android.library)
     alias(libs.plugins.practicalchristian.hilt)
     alias(libs.plugins.practicalchristian.android.library.compose)
 }
 
-android {
+configure<LibraryExtension> {
     namespace = "com.sacrament.ui"
 }
 
